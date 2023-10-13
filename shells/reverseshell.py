137e0addea08ff7d9fd8931b65ed699f6ceee8bc
HK Transfield <h.k.transfield1@gmail.com>
2023-10-13 22:12:51 +1300

    refactor: add menus module to print useful info

--- shells/reverseshell.py
+++ shells/reverseshell.py
@@ -1,19 +1,10 @@
+from menus import bcolors
+
 import os
 import shutil
 import subprocess
 import random
 import string
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def create_windows_payload(host_ip, host_port):
 
--- tempest.py
+++ tempest.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from menus import banner, help, bcolors
 from prettytable import PrettyTable
 from shells import reverseshell
 
@@ -19,48 +20,6 @@ TODO Somethings I can try out:
 
 
 BUFFER = 1024
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def banner():
-    """ Prints an ASCII art title banner. Intended to use when starting the server.
-    """
-    print(bcolors.HEADER + '=============================================' + bcolors.ENDC)
-    print(bcolors.HEADER + ' _____  _____ _      ____  _____ ____  _____ ' + bcolors.ENDC)
-    print(bcolors.HEADER + '/__ __\/  __// \__/|/  __\/  __// ___\/__ __\\' + bcolors.ENDC)
-    print(bcolors.HEADER + '  / \  |  \  | |\/|||  \/||  \  |    \  / \  ' + bcolors.ENDC)
-    print(bcolors.HEADER + '  | |  |  /_ | |  |||  __/|  /_ \___ |  | |  ' + bcolors.ENDC)
-    print(bcolors.HEADER + '  \_/  \____\\\\_/  \|\_/   \____\\\\____/  \_/ ' + bcolors.ENDC)
-    print(bcolors.HEADER + bcolors.BOLD + '\nCreated By HK Transfield' + bcolors.ENDC)
-    print(bcolors.HEADER + '=============================================\n' + bcolors.ENDC)
-
-def help():
-    print('''
-
-    Menu Commands
-    ----------------------------------------------------------------------------------------
-    listeners -g            ---> Generate a new listener
-    winplant py             ---> Generate a Windows compatible Python payload
-    unixplant py            ---> Generate a Linux compatible Python payload
-    exeplant                ---> Generate an executable payload for windows
-    sessions -l             ---> List sessions
-    sessions -i <id>        ---> Interact with a new sessions
-    kill <id>               ---> Kills an active session
-
-    Session Commands
-    ----------------------------------------------------------------------------------------
-    background              ---> Backgrounds the current session
-    exit                    ---> Terminates the current session
-    ''')
 
 def comms_in(target_id):
     """ Handles all responses sent from a sockclient.
